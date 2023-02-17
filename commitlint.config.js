f778330fa394d7b5027a6176a228aeffcc6740f0
Gillian Tunney <gbtunney@mac.com>
2023-02-17 18:56:36 -0500

    feat(root): updated files

--- commitlint.config.js
+++ commitlint.config.js
@@ -8,6 +8,6 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'scope-empty': [2, 'never'],
-        'scope-enum': [2, 'always', ['root', 'release']],
+        'scope-enum': [2, 'always', ['root', 'release', 'sat-scripts']],
     },
 }
