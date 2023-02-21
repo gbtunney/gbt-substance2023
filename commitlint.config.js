b18cfc51414df0a15dc64caed3c5a3b498095ab7
Gillian Tunney <gbtunney@mac.com>
2023-02-20 19:12:47 -0500

    feat(root): added new packages

--- commitlint.config.js
+++ commitlint.config.js
@@ -8,6 +8,16 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'scope-empty': [2, 'never'],
-        'scope-enum': [2, 'always', ['root', 'release', 'sat-scripts']],
+        'scope-enum': [
+            2,
+            'always',
+            [
+                'root',
+                'release',
+                'sat-scripts',
+                'value-processor-utilities',
+                'palatte',
+            ],
+        ],
     },
 }
