ed90e84b4cff347e01624b2b66aa7996eb23c733
Gillian Tunney <gbtunney@mac.com>
2023-02-21 18:17:31 -0500

    refactor(root): random stuff

--- packages/sat-scripts/prettier.config.cjs
+++ packages/sat-scripts/prettier.config.cjs
@@ -11,6 +11,7 @@ module.exports = merge(Prettier.config, {
             options: {
                 parser: 'xml',
                 tabWidth: 2,
+                xmlWhitespaceSensitivity: 'ignore',
             },
         },
     ],
